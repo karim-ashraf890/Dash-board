65d30fa373a3651813a743fc39e5d8bc76c195df
karim <karimashraf890@outlook.com>
2025-11-13 16:01:46 +0200

    updateButton

--- pages/login/login.ts
+++ pages/login/login.ts
@@ -2,49 +2,70 @@ import { isEmail, removeError, showError } from '../../helpers/helper';
 import '../../global.scss';
 import './login.scss';
 
+let errors: { email: string[]; password: string[] } = {
+    email: [],
+    password: [],
+};
 
 let emailSignIn = document.getElementById("signIn") as HTMLInputElement;
 emailSignIn.addEventListener("keyup", addAndRemoveErrorEmail);
 function addAndRemoveErrorEmail() {
+    errors.email = [];
     let emailValue = emailSignIn.value;
-    let messages: string[] = [];
     if (isEmail(emailValue)) {
-        removeError("signIn")
+        removeError("signIn");
     } else {
         if (emailValue != '')
-            messages.push("Wrong email");
+            errors.email.push("Wrong email");
     }
 
     if (emailValue == '') {
-        messages.push("Must enter this field");
+        errors.email.push("Must enter this field");
     }
 
-    if (messages.length > 0) {
-        showError("signIn", messages);
-    }
+    if (errors.email.length > 0) {
+        showError("signIn", errors.email);
+    };
+    updateButton();
 };
+
 let Password = document.getElementById("Password") as HTMLInputElement;
 Password.addEventListener("keyup", addAndRemoveErrorPassword);
 function addAndRemoveErrorPassword() {
+    errors.password = [];
     let PasswordValue = Password.value;
-    let messages: string[] = [];
     if (PasswordValue.length >= 5) {
         removeError("Password")
     } else {
         if (PasswordValue.length !== 0)
-            messages.push("Password should be more than 5 length");
+            errors.password.push("Password should be more than 5 length");
     }
 
     if (PasswordValue.length == 0) {
-        messages.push("Password should be more than 5 length");
-        messages.push("Must enter this field");
-
+        errors.password.push("Password should be more than 5 length");
+        errors.password.push("Must enter this field");
     }
 
-    if (messages.length > 0) {
-        showError("Password", messages);
+    if (errors.password.length > 0) {
+        showError("Password", errors.password);
+    };
+    updateButton();
+};
+
+const loginButton = document.getElementById("login") as HTMLButtonElement;
+
+function updateButton() {
+    const containsErrors = errors.email.length > 0 || errors.password.length > 0;
+    const noErrors = emailSignIn.value !== "" && Password.value !== "";
+
+    if (containsErrors || !noErrors) {
+        loginButton.disabled = true;
+    } else {
+        loginButton.disabled = false;
     }
 }
+
+
 
 
 
